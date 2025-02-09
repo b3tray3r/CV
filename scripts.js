1300d6efec9e43cda17991edaec137056aa8d052
b3tray3r <40644354+b3tray3r@users.noreply.github.com>
2025-02-09 20:41:19 +0300

    scripts.js

--- scripts.js
+++ scripts.js
@@ -1,5 +1,19 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const image = document.getElementById("data-image");
-    image.style.opacity = 0;
-    setTimeout(() => { image.style.opacity = 1; }, 1000);
+    const sections = document.querySelectorAll(".animated-section");
+    const titles = document.querySelectorAll("h2");
+
+    function revealOnScroll() {
+        sections.forEach((section, index) => {
+            const sectionTop = section.getBoundingClientRect().top;
+            const windowHeight = window.innerHeight;
+
+            if (sectionTop < windowHeight - 100) {
+                section.classList.add("visible");
+                titles[index].classList.add("visible"); // Добавляем класс заголовку
+            }
+        });
+    }
+
+    window.addEventListener("scroll", revealOnScroll);
+    revealOnScroll(); // Запускаем на старте, если блоки уже в зоне видимости
 });
